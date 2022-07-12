120ab5c7464079e75b79f65639a546e670063726
Artem Derbin <artemsdft@mail.ru>
2022-07-12 14:24:46 +0300

    add Model

---
+++ Home Work 15 MVC/App/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Home Work 15 MVC
+//
+//  Created by Артем Дербин on 12.07.2022.
+//
+
+import UIKit
+
+ @main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        let viewController = ViewController()
+        var navigationController = UINavigationController()
+
+        navigationController = UINavigationController(rootViewController: viewController)
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
